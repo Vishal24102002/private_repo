d682e75ff58fed52a0746eec56bc606c5128371f
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-11-17 17:55:59 +0000

    modified:   assets/js/booking.js 	modified:   booking.html

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,9 +1,28 @@
 function getSelectedcheckinDate() {
     // Get the date input element
-    const dateInput = document.getElementsByClassName("date-input-check-in")
+    const dateInput = document.getElementById("date-input-check-in")
+
     // Get the selected date value
     const selectedDate = dateInput.value;
+    const date = new Date(selectedDate);
+
+  // Array of month names
+  const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "June",
+    "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+  ];
+
+  // Get the month name
+  const monthName = monthNames[date.getMonth()]; // getMonth() returns the month index (0-11)
+
+  // Construct the formatted date string
+  const formattedDate = `${monthName} ${date.getDate()},${date.getFullYear()}`;
+
+  // Display the formatted date in the label
+  document.getElementById("Check-in-label").innerText = formattedDate;
+
+    // document.getElementById("Check-in-label").innerText= selectedDate
   
     // Display the selected date
-    alert("Selected Date: " + selectedDate);
+    alert("Selected Date: " + formattedDate);
   }
