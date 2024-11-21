fa4c75ce31ec6823e6590932a40cb18444e124b6
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2024-11-21 06:16:14 +0000

    modified:   assets/js/booking.js 	modified:   booking.html

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,3 +1,48 @@
+const moreDetailsBtn = document.getElementById("moreDetails1");
+const popup = document.getElementById("popup_basic");
+const closePopupBtn = document.getElementById("closePopup");
+    
+const moreDetailsBtn2 = document.getElementById("moreDetails2");
+const popup2 = document.getElementById("popup_infinity");
+const closePopupBtn2 = document.getElementById("closePopup2");
+document.getElementById("select_infi").addEventListener("click",(e) =>{
+  e.preventDefault();
+  document.getElementById("room_price").innerText= 7600
+});
+
+document.getElementById("select_basic").addEventListener("click",(e) =>{
+  e.preventDefault();
+  document.getElementById("room_price").innerText= 3600
+});
+// Show popup
+moreDetailsBtn.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent the default anchor behavior
+    popup.style.display = "flex";
+});
+moreDetailsBtn2.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent the default anchor behavior
+    popup2.style.display = "flex";
+});
+// Close popup
+closePopupBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+});
+closePopupBtn2.addEventListener("click", () => {
+    popup2.style.display = "none";
+});
+
+// Close popup when clicking outside the content
+popup.addEventListener("click", (e) => {
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+});
+popup2.addEventListener("click", (e) => {
+    if (e.target === popup2) {
+        popup2.style.display = "none";
+    }
+ });
+
 function getSelectedcheckinDate() {
     // Get the date input element
     const indateInput = document.getElementById("date-input-check-in")
@@ -37,6 +82,7 @@ function getSelectedcheckinDate() {
       document.getElementById("room_rev").innerText = `${selectedValue} ${outdate.getDate()-indate.getDate()} night`;
       document.getElementById("Check-in-label").innerText = formattedinDate;
       document.getElementById("Check-out-label").innerText = formattedoutDate;
+      document.getElementById("room_price").innerText= `${document.getElementById("room_price").innerText*(outdate.getDate()-indate.getDate())}`;
       alert("review the registration details before moving forward as changes in dates are not entertained");
     }
     
